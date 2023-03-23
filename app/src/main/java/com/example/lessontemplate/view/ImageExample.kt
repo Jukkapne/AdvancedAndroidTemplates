38f916c14405f51a2e1fbca94061753879761c70
repenaattori <reimarii@oamk.fi>
2023-03-23 10:54:36 +0200

    Added image fetch from the firebase storage

---
+++ app/src/main/java/com/example/lessontemplate/view/ImageExample.kt
@@ -0,0 +1,38 @@
+package com.example.lessontemplate.view
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.platform.LocalContext
+import coil.ImageLoader
+import coil.compose.rememberAsyncImagePainter
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
+
+
+@Composable
+fun ShowStorageImage() {
+    var byteArray by remember { mutableStateOf<ByteArray?>(null) }
+
+    var painter = rememberAsyncImagePainter(
+        model = byteArray
+    )
+
+    Column() {
+        Button(onClick = {
+            Firebase.storage.reference
+                .child("moth.jpg")
+                .getBytes(1024*1024)
+                .addOnSuccessListener {
+                    byteArray = it
+                }
+        }) {
+            Text(text = "Get image")
+        }
+        Image(painter = painter , contentDescription = "images")
+    }
+    
+
+}
