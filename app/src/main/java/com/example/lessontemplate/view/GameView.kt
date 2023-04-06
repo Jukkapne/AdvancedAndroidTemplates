fc7c0f7d01326e7677889180bd6c22d7273c3231
reimarii <reimarii@oamk.fi>
2023-04-06 08:58:02 +0300

    Refactoring code to a cleaner solution

--- app/src/main/java/com/example/lessontemplate/view/GameView.kt
+++ app/src/main/java/com/example/lessontemplate/view/GameView.kt
@@ -7,10 +7,13 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Button
 import androidx.compose.material.Card
+import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
@@ -37,23 +40,34 @@ fun GameView() {
     val vm: GameViewModel = viewModel()
     val navController = rememberNavController()
 
+    //Function for selecting game by id and navigating to game info view
+    val selectGame: (Int) -> Unit = {gameId ->
+        vm.getGameById(gameId)
+        vm.getReviews(gameId.toString())
+        navController.navigate(Route.GameInfo.name)
+    }
+
+
+    //Function for adding review text for game with id
+    val addReview: (String, String) -> Unit = { id, text ->
+        vm.addReview(id, text)
+    }
+
+    //Function for navigating to the game list
+    val showGame: () -> Unit = { navController.navigate(Route.Games.name) }
+
     NavHost(navController = navController, startDestination = Route.Games.name){
         composable(Route.Games.name){
-            //Passing the function as last parameter to provide the GameList
-            //means of accessing viewmodel getGameById
-            GameList(games = vm.games ){gameId ->
-                navController.navigate( "${Route.GameInfo.name}/${gameId}" )
-            }
+            GameList(vm.games, selectGame)
         }
-        composable(
-            "${Route.GameInfo.name}/{gameId}",
-            arguments = listOf(navArgument("gameId"){type= NavType.IntType})
-        ){navEntry ->
-            vm.getGameById(navEntry.arguments?.getInt("gameId"))
-            vm.game.value?.let {
-                GameInfo(it) {
-                    navController.navigate(Route.Games.name)
-                }
+        composable(Route.GameInfo.name){
+            vm.game.value?.let { game ->
+                GameInfo(
+                    game,
+                    vm.reviews,
+                    addReview,
+                    showGame
+                )
             }
         }
     }
@@ -78,29 +92,56 @@ fun GameList(games: List<Game>, selectGame: (id: Int) -> Unit) {
 
 
 @Composable
-fun GameInfo(game: Game, showGames: () -> Unit) {
+fun GameInfo(
+    game: Game,
+    reviews: List<String>,
+    addReview: (String, String) -> Unit,
+    showGames: () -> Unit
+) {
 
+    var reviewText by remember { mutableStateOf<String>("") }
     val imageUrl = if(game.screenshots.isNotEmpty()) game.screenshots[0].image else game.thumbnail
 
-    Card(
-        elevation = 10.dp, modifier = Modifier
-            .fillMaxWidth()
-    ) {
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally,
-            modifier = Modifier.padding(10.dp)
-        ) {
+    Column(modifier = Modifier.padding(10.dp)) {
+        Card( elevation = 10.dp, modifier = Modifier.fillMaxWidth()) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier.padding(10.dp)
+            ) {
+                Image(
+                    painter = rememberAsyncImagePainter(model = imageUrl),
+                    contentDescription = "game image",
+                    modifier = Modifier
+                        .clickable { showGames() }
+                        .padding(5.dp)
+                        .border(width = 2.dp, color = Color.Black)
+                )
 
-            Image(
-                painter = rememberAsyncImagePainter(model = imageUrl),
-                contentDescription = "game image",
-                modifier = Modifier
-                    .clickable { showGames() }
-                    .padding(5.dp)
-                    .border(width = 2.dp, color = Color.Black)
+                Text(text = game.title, fontSize = 18.sp, fontWeight = FontWeight.Bold)
+            }
+        }
+        Text(text = "Add review")
+        OutlinedTextField(
+            value = reviewText,
+            onValueChange = {reviewText = it},
+            modifier = Modifier.height(200.dp)
+        )
+        Button(onClick = { addReview(game.id.toString(), reviewText) }) {
+            Text(text = "Submit review")
+        }
+        Reviews(reviews)
+    }
+}
+
+@Composable
+fun Reviews(reviews: List<String>) {
+    LazyColumn(){
+        items(reviews){
+            Text(text = it.toString(), modifier = Modifier
+                .padding(5.dp)
+                .border(width = 1.dp, color = Color.Black)
+                .padding(5.dp)
             )
-
-            Text(text = game.title, fontSize = 18.sp, fontWeight = FontWeight.Bold)
         }
     }
 }
