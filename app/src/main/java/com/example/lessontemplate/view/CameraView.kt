752bca678513ace7133578b84e4cf054f876c873
reimarii <reimarii@oamk.fi>
2023-04-12 14:34:39 +0300

    Permissions

---
+++ app/src/main/java/com/example/lessontemplate/view/CameraView.kt
@@ -0,0 +1,59 @@
+package com.example.lessontemplate.view
+
+import android.Manifest
+import android.util.Log
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.SideEffect
+import com.google.accompanist.permissions.*
+
+@OptIn(ExperimentalPermissionsApi::class)
+@Composable
+fun MainCameraView() {
+    val permissionState = rememberPermissionState(permission = Manifest.permission.CAMERA)
+
+    when(permissionState.status){
+        is PermissionStatus.Granted -> {
+            Text(text = "Permission to camera granted")
+        }
+        else -> {
+            if (permissionState.status.shouldShowRationale) {
+                //Denied once and should show the dialog again
+                Text(text = "You should really give permission to use this feature")
+
+            } else {
+                //When first time starting the application and if permission denied twice (or in setting)
+                Text(text = "You've denied permission for camera")
+            }
+            SideEffect {
+                permissionState.launchPermissionRequest()
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalPermissionsApi::class)
+@Composable
+fun CameraAndRecording() {
+    val permissionMultipleState = rememberMultiplePermissionsState(
+        permissions = listOf(
+            Manifest.permission.CAMERA,
+            Manifest.permission.RECORD_AUDIO
+        )
+    )
+
+    if(permissionMultipleState.allPermissionsGranted){
+        Log.e("", "Permissions granted")
+    }else{
+        if(permissionMultipleState.shouldShowRationale){
+            Log.e("", "Permissions rationale")
+        }else{
+            Log.e("", "Permissions denied")
+        }
+        SideEffect {
+            permissionMultipleState.launchMultiplePermissionRequest()
+        }
+    }
+
+}
